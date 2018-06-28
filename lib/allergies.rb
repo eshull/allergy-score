8db8f90bbd49afc73cfcc2ce64a7177791dc9683
Eric Shull and Nate Cottle <nate@>
2018-06-28 16:40:25 -0700

    final commit

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,37 +1,38 @@
 class Allergy
-  attr_reader(:score)
   def initialize (score)
     @score = score
   end
   def score_checker
     allergy_array = []
     until @score == 0 do
-    if @score >= 128
-      allergy_array.push('cats')
-      @score -= 128
-    elsif @score >=64
-      allergy_array.push('pollen')
-      @score -= 64
-    elsif @score >=32
-      allergy_array.push('chocolate')
-      @score -= 32
-    elsif @score >=16
-      allergy_array.push('tomatoes')
-      @score -= 16
-    elsif @score >=8
-      allergy_array.push('strawberries')
-      @score -= 8
-    elsif @score >=4
-      allergy_array.push('shellfish')
-      @score -= 4
-    elsif @score >=2
-      allergy_array.push('peanuts')
-      @score -= 2
-    elsif @score >=1
-      allergy_array.push('eggs')
-      @score -= 1
+      if @score > 255
+        @score == 0
+      elsif @score >= 128
+        allergy_array.push('cats')
+        @score -= 128
+      elsif @score >=64
+        allergy_array.push('pollen')
+        @score -= 64
+      elsif @score >=32
+        allergy_array.push('chocolate')
+        @score -= 32
+      elsif @score >=16
+        allergy_array.push('tomatoes')
+        @score -= 16
+      elsif @score >=8
+        allergy_array.push('strawberries')
+        @score -= 8
+      elsif @score >=4
+        allergy_array.push('shellfish')
+        @score -= 4
+      elsif @score >=2
+        allergy_array.push('peanuts')
+        @score -= 2
+      elsif @score >=1
+        allergy_array.push('eggs')
+        @score -= 1
+      end
     end
-  end
     allergy_array
   end
 end
