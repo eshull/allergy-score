8f2e95d47180396d6e06c0bcbc17125cdb5bddfc
Eric Shull and Nate Cottle <nate@>
2018-06-28 14:32:35 -0700

    add second positive fail

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -5,10 +5,15 @@ class Allergy
   end
   def score_checker
     allergy_array = []
+    until @score == 0 do
     if @score >= 128
       allergy_array.push('cats')
       @score -= 128
+    elsif @score >=64
+      allergy_array.push('pollen')
+      @score -= 64
     end
+  end
     allergy_array
   end
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -10,4 +10,8 @@ describe('#allergies') do
     person = Allergy.new(128)
     expect(person.score_checker).to(eq(["cats"]))
   end
+  it('allergy score is 64') do
+    person = Allergy.new(64)
+    expect(person.score_checker).to(eq(["cats"]))
+  end
 end
